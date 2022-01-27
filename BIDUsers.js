0eea8e429852fca820d3fb9b01309745aa1a0ce0
Manik <manik@1kosmos.com>
2022-01-27 16:57:56 -0500

    no message

--- BIDUsers.js
+++ BIDUsers.js
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2018, 1Kosmos Inc. All rights reserved.
+ * Licensed under 1Kosmos Open Source Public License version 1.0 (the "License");
+ * You may not use this file except in compliance with the License. 
+ * You may obtain a copy of this license at 
+ *    https://github.com/1Kosmos/1Kosmos_License/blob/main/LICENSE.txt
+ */
 "use strict";
 const { v4: uuidv4 } = require('uuid');
 const BIDECDSA = require('./BIDECDSA');
