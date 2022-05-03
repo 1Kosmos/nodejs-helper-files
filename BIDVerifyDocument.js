db9ddd6bbc886316a8946071324f3152bda42086
jenish <jenishptrt@gmail.com>
2022-05-03 11:24:27 +0530

    added function for create session for document verification

--- BIDVerifyDocument.js
+++ BIDVerifyDocument.js
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) 2018, 1Kosmos Inc. All rights reserved.
+ * Licensed under 1Kosmos Open Source Public License version 1.0 (the "License");
+ * You may not use this file except in compliance with the License. 
+ * You may obtain a copy of this license at 
+ *    https://github.com/1Kosmos/1Kosmos_License/blob/main/LICENSE.txt
+ */
+
 "use strict";
 const { v4: uuidv4 } = require('uuid');
 const NodeCache = require('node-cache');
@@ -8,10 +16,11 @@ const fetch = require('node-fetch');
 
 const cache = new NodeCache({ stdTTL: 10 * 60 });
 
+const smsTemplate = "Hello Developer, Please complete the document scan for <tenantname> by going to <link>"
+
 const getDocVerifyPublicKey = async () => {
   try {
     const sd = await BIDSDK.getSD();
-    console.log('sd:', sd);
     let docVerifyPublicKeyCache = cache.get(sd.docuverify + "/publickeys");
 
     if (docVerifyPublicKeyCache) {
@@ -43,9 +52,6 @@ const getDocVerifyPublicKey = async () => {
 
 const verifyDocument = async (dvcId, verifications, document) => {
   try {
-    console.log('dvcId:', dvcId);
-    console.log('verifications:', verifications);
-    console.log('document:', document)
     const keySet = BIDSDK.getKeySet();
     const licenseKey = BIDSDK.getLicense();
     const sd = await BIDSDK.getSD();
@@ -94,6 +100,134 @@ const verifyDocument = async (dvcId, verifications, document) => {
   }
 }
 
+const sendSMS = async (communityInfo, smsTo, smsISDCode, smsTemplateB64, requestIdUuid) => {
+  try {
+    const keySet = BIDSDK.getKeySet();
+    const licenseKey = BIDSDK.getLicense();
+    const sd = await BIDSDK.getSD();
+
+    const {
+      community:
+      {
+        publicKey: communityPublicKey,
+        tenantid: tenantId,
+        id: communityId
+      },
+      tenant: {
+        tenanttag: tenantTag
+      }
+    } = communityInfo;
+
+    let sharedKey = BIDECDSA.createSharedKey(keySet.prKey, communityPublicKey);
+
+    const encryptedRequestId = BIDECDSA.encrypt(JSON.stringify({
+      ts: Math.round(new Date().getTime() / 1000),
+      appid: 'fixme',
+      uuid: requestIdUuid
+    }), sharedKey);
+
+    let headers = {
+      'Content-Type': 'application/json',
+      'charset': 'utf-8',
+      'X-TenantTag': tenantTag,
+      publickey: keySet.pKey,
+      licensekey: BIDECDSA.encrypt(licenseKey, sharedKey),
+      requestid: encryptedRequestId
+    };
+
+    const req = {
+      tenantId,
+      communityId,
+      smsTo,
+      smsISDCode,
+      smsTemplateB64
+    };
+
+    let api_response = await fetch(sd.adminconsole + "/api/r2/messaging/schedule", {
+      method: 'post',
+      body: JSON.stringify(req),
+      headers: headers
+    });
+
+    if (api_response) {
+      api_response = await api_response.json();
+    }
+
+    return api_response;
+
+  } catch (error) {
+    throw error;
+  }
+}
+
+const createDocumentSession = async (dvcID, documentType, smsTo, smsISDCode) => {
+  try {
+    const keySet = BIDSDK.getKeySet();
+    const licenseKey = BIDSDK.getLicense();
+    const sd = await BIDSDK.getSD();
+    const docVerifyPublicKey = await getDocVerifyPublicKey();
+    const communityInfo = await BIDSDK.getCommunityInfo();
+    const tenantInfo = BIDSDK.getTenant();
+
+    let sharedKey = BIDECDSA.createSharedKey(keySet.prKey, docVerifyPublicKey);
+    let requestIdUuid = uuidv4();
+    let userUIDAndDid = uuidv4();
+
+    const encryptedRequestId = BIDECDSA.encrypt(JSON.stringify({
+      ts: Math.round(new Date().getTime() / 1000),
+      appid: 'fixme',
+      uuid: requestIdUuid
+    }), sharedKey);
+
+    const headers = {
+      'Content-Type': 'application/json',
+      charset: 'utf-8',
+      publickey: keySet.pKey,
+      licensekey: BIDECDSA.encrypt(licenseKey, sharedKey),
+      requestid: encryptedRequestId
+    };
+
+    const req = {
+      dvcID,
+      sessionRequest: {
+        tenantDNS: tenantInfo.dns,
+        communityName: communityInfo.community.name,
+        documentType,
+        userUID: userUIDAndDid,
+        did: userUIDAndDid
+      }
+    };
+
+    const encryptedData = {
+      data: BIDECDSA.encrypt(JSON.stringify(req), sharedKey)
+    };
+
+    let api_response = await fetch(sd.docuverify + "/document_share_session/create", {
+      method: 'post',
+      body: JSON.stringify(encryptedData),
+      headers: headers
+    });
+
+    if (api_response) {
+      api_response = await api_response.json();
+    }
+
+    const templateText = smsTemplate
+      .toString()
+      .replace(/<tenantname>/, communityInfo.tenant.name)
+      .replace(/<link>/, api_response.url);
+    
+    const smsTemplateB64 = Buffer.from(templateText).toString('base64');
+
+    await sendSMS(communityInfo, smsTo, smsISDCode, smsTemplateB64, requestIdUuid);
+    return ({ sessionId: api_response.sessionId, sessionUrl: api_response.url });
+
+  } catch (error) {
+    throw error;
+  }
+}
+
 module.exports = {
-  verifyDocument
+  verifyDocument,
+  createDocumentSession
 }
