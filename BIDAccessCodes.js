cf4dec1b1501e1588d5e25ed8cb87546be4bc7e4
jenish <jenishptrt@gmail.com>
2022-06-10 12:54:33 +0530

    Added verify and redeem email verification code

--- BIDAccessCodes.js
+++ BIDAccessCodes.js
@@ -97,6 +97,140 @@ const requestEmailVerificationLink = async (tenantInfo, emailTo, emailTemplateB6
     }
 }
 
+const fetchAccessCode = async (tenantInfo, code) => {
+    try {
+        const communityInfo = await BIDTenant.getCommunityInfo(tenantInfo);
+        const keySet = BIDTenant.getKeySet();
+        const licenseKey = tenantInfo.licenseKey;
+        const sd = await BIDTenant.getSD(tenantInfo);
+
+        const {
+            community: {
+                publicKey: communityPublicKey,
+                name: communityName
+            },
+            tenant: {
+                tenanttag: tenantTag
+            }
+        } = communityInfo;
+
+        let sharedKey = BIDECDSA.createSharedKey(keySet.prKey, communityPublicKey);
+
+        const encryptedRequestId = BIDECDSA.encrypt(JSON.stringify({
+            ts: Math.round(new Date().getTime() / 1000),
+            appid: 'fixme',
+            uuid: uuidv4()
+        }), sharedKey);
+
+        let headers = {
+            'Content-Type': 'application/json',
+            'charset': 'utf-8',
+            'X-tenantTag': tenantTag,
+            publickey: keySet.pKey,
+            licensekey: BIDECDSA.encrypt(licenseKey, sharedKey),
+            requestid: encryptedRequestId
+        };
+
+        let api_response = await fetch(sd.adminconsole + "/api/r1/acr/community/" + communityName + "/" + code, {
+            method: 'get',
+            headers: headers
+        });
+
+        let status = api_response.status;
+
+        if (api_response) {
+            api_response = await api_response.json();
+            if (api_response.data) {
+                let dec_data = BIDECDSA.decrypt(api_response.data, sharedKey);
+                api_response = JSON.parse(dec_data);
+            }
+        }
+
+        api_response.statusCode = status;
+
+        return api_response;
+
+    } catch (error) {
+        throw error;
+    }
+}
+
+const verifyAndRedeemEmailVerificationCode = async (tenantInfo, code) => {
+    try {
+
+        if (!code) {
+            return { statusCode: 400, message: "code is required parameter" }
+        }
+
+        const communityInfo = await BIDTenant.getCommunityInfo(tenantInfo);
+        const licenseKey = tenantInfo.licenseKey;
+        const sd = await BIDTenant.getSD(tenantInfo);
+        const keySet = BIDTenant.getKeySet();
+
+        const {
+            community: {
+                publicKey: communityPublicKey,
+                name: communityName
+            },
+            tenant: {
+                tenanttag: tenantTag
+            }
+        } = communityInfo;
+
+        let access_code_response = await fetchAccessCode(tenantInfo, code);
+
+        if (access_code_response.statusCode !== 200) {
+            return access_code_response;
+        }
+
+        if (access_code_response.type !== 'verification_link') {
+            return { statusCode: 400, message: "Provided verification code is invalid" };
+        }
+
+        let sharedKey = BIDECDSA.createSharedKey(keySet.prKey, communityPublicKey);
+
+        const encryptedRequestId = BIDECDSA.encrypt(JSON.stringify({
+            ts: Math.round(new Date().getTime() / 1000),
+            appid: 'fixme',
+            uuid: uuidv4()
+        }), sharedKey);
+
+        let headers = {
+            'Content-Type': 'application/json',
+            'charset': 'utf-8',
+            'X-tenantTag': tenantTag,
+            publickey: keySet.pKey,
+            licensekey: BIDECDSA.encrypt(licenseKey, sharedKey),
+            requestid: encryptedRequestId
+        };
+
+        let api_response = await fetch(sd.adminconsole + "/api/r1/acr/community/" + communityName + "/" + code + "/redeem", {
+            method: 'post',
+            body: JSON.stringify({}),
+            headers: headers
+        });
+
+        let status = api_response.status;
+
+        if (api_response) {
+            api_response = await api_response.json();
+        }
+
+        if (status === 200) {
+            api_response = { ...api_response, ...access_code_response };
+            api_response.status = "redeemed";
+        }
+
+        api_response.statusCode = status;
+
+        return api_response;
+
+    } catch (error) {
+        throw error;
+    }
+}
+
 module.exports = {
-    requestEmailVerificationLink
+    requestEmailVerificationLink,
+    verifyAndRedeemEmailVerificationCode
 }
