02bc8136179c3aec804a606c5daff6fe2843e9aa
jenish patel <jenish4096@gmail.com>
2022-11-14 14:54:40 +0530

    implemented verify verifiable credentials

--- BIDVerifiableCredential.js
+++ BIDVerifiableCredential.js
@@ -84,11 +84,50 @@ const issueVerifiableCredentials = async (tenantInfo, type, document) => {
             }
         });
 
-        let status = api_response.status;
+        api_response = api_response.json;
+
+        return api_response;
+    } catch (error) {
+        throw error;
+    }
+}
+
+const verifyVerifiableCredentials = async (tenantInfo, issuedVerifiableCredentials) => {
+    try {
+        const communityInfo = await BIDTenant.getCommunityInfo(tenantInfo);
+        const keySet = BIDTenant.getKeySet();
+        const licenseKey = tenantInfo.licenseKey;
+        const sd = await BIDTenant.getSD(tenantInfo);
+
+        let sessionsPublicKey = await getVcsPublicKey(tenantInfo);
+        
+        let sharedKey = BIDECDSA.createSharedKey(keySet.prKey, sessionsPublicKey);
+
+        const encryptedRequestId = BIDECDSA.encrypt(JSON.stringify({
+            ts: Math.round(new Date().getTime() / 1000),
+            appid: 'fixme',
+            uuid: uuidv4()
+        }), sharedKey);
+
+        let headers = {
+            'Content-Type': 'application/json',
+            'charset': 'utf-8',
+            publickey: keySet.pKey,
+            licensekey: BIDECDSA.encrypt(licenseKey, sharedKey),
+            requestid: encryptedRequestId
+        }
+
+        let api_response = await WTM.executeRequest({
+            method: 'post',
+            url: sd.vcs + "/tenant/" + communityInfo.tenant.id + "/community/" + communityInfo.community.id + "/verify/vc",
+            headers,
+            body: {
+                vc: issuedVerifiableCredentials
+            }
+        });
 
         api_response = api_response.json;
 
-        api_response.status = status;
         return api_response;
     } catch (error) {
         throw error;
@@ -96,5 +135,6 @@ const issueVerifiableCredentials = async (tenantInfo, type, document) => {
 }
 
 module.exports = {
-    issueVerifiableCredentials
+    issueVerifiableCredentials,
+    verifyVerifiableCredentials
 }
