5d71b7ee2f7bbaeaa72e3b4657c30e8f7094d56d
Elamaran Anbazhagan <elamaran.a@1kosmos.com>
2025-07-31 17:11:48 +0530

    "testing new script"

--- BIDECDSA.js
+++ BIDECDSA.js
@@ -128,30 +128,42 @@ module.exports = {
   },
 
   toBase64Url: function (buf) {
-    return buf.toString('base64').replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '');
+    return buf
+      .toString("base64")
+      .replace(/\+/g, "-")
+      .replace(/\//g, "_")
+      .replace(/=+$/, "");
   },
 
-  sign: function (data, privateKeyBase64) {
-    const privKey = Buffer.from(privateKeyBase64, 'base64').toString('hex');
-    const key = ec.keyFromPrivate(privKey, 'hex');
-    const hash = crypto.createHash('sha256').update(data).digest();
-    const signature = key.sign(hash, { canonical: true });
-    const sigBytes = Buffer.concat([
-      signature.r.toArrayLike(Buffer, 'be', 32),
-      signature.s.toArrayLike(Buffer, 'be', 32),
-    ]);
-    return sigBytes.toString('base64');
+  base64ToBuffer(base64) {
+    return Buffer.from(base64, "base64");
   },
 
-  verify: function (data, signatureBase64, publicKeyBase64) {
-    const pub = Buffer.from(publicKeyBase64, 'base64');
-    const x = pub.slice(0, 32).toString('hex');
-    const y = pub.slice(32, 64).toString('hex');
-    const key = ec.keyFromPublic({ x, y }, 'hex');
-    const hash = crypto.createHash('sha256').update(data).digest();
-    const signature = Buffer.from(signatureBase64, 'base64');
-    const r = signature.slice(0, 32);
-    const s = signature.slice(32, 64);
+  sign(message, privateKeyBase64) {
+    const privBuf = this.base64ToBuffer(privateKeyBase64);
+    if (privBuf.length !== 32) throw new Error("Invalid private key length");
+
+    const key = ec.keyFromPrivate(privBuf);
+    const hash = crypto.createHash("sha256").update(message).digest();
+    const signature = key.sign(hash);
+
+    const r = signature.r.toArrayLike(Buffer, "be", 32);
+    const s = signature.s.toArrayLike(Buffer, "be", 32);
+    return Buffer.concat([r, s]).toString("base64");
+  },
+
+  verify(message, signatureBase64, publicKeyBase64) {
+    const sigBuf = this.base64ToBuffer(signatureBase64);
+    const pubBuf = this.base64ToBuffer(publicKeyBase64);
+    if (pubBuf.length !== 64) throw new Error("Invalid public key length");
+
+    const x = pubBuf.slice(0, 32).toString("hex");
+    const y = pubBuf.slice(32).toString("hex");
+    const key = ec.keyFromPublic({ x, y }, "hex");
+
+    const hash = crypto.createHash("sha256").update(message).digest();
+    const r = sigBuf.slice(0, 32);
+    const s = sigBuf.slice(32);
     return key.verify(hash, { r, s });
-  }
-} 
+  },
+};
