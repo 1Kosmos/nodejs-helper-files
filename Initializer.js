3a8beb7eb524c405ff019944d4d64e9b2811167e
Pankti Shah <pankti.shah@1kosmos.com>
2025-09-15 20:48:28 +0530

    fetch and set globally EC_CURVE_NAME

--- Initializer.js
+++ Initializer.js
@@ -1,7 +1,6 @@
 'use strict';
 const ResponseError = require('./ResponseError');
 const {ResponseCodes} = require('./ResponseCodes');
-const BIDCaas = require('./BIDCaas');
 
 /**
  * Initializer using module pattern since language constructs prevent
@@ -9,8 +8,7 @@ const BIDCaas = require('./BIDCaas');
  */
 const Initializer = (function() {
     var _origination = undefined;
-    var _ecCurveName = undefined;
-
+  
     return { // public interface
       setOrigination: function (origination) {
         if(_origination)
@@ -23,32 +21,6 @@ const Initializer = (function() {
       getOrigination: function () {
         if(!_origination) return "service_uninitialized";
         return _origination;
-      },
-
-        setECCurveName: function (curveName) {
-            _ecCurveName = curveName;
-        },
-
-        getECCurveName: function () {
-            if (!_ecCurveName) {
-                console.error('ERROR: EC_CURVE_NAME missing or empty');
-                process.exit(1);
-            }
-            return _ecCurveName;
-        },
-
-        fetchAndInitECCurveName: async function (apiUrl) {
-            try {
-                const response = await BIDCaas.getEnvironment(apiUrl);
-                if (!response || !response.EC_CURVE_NAME) {
-                    console.error('ERROR: EC_CURVE_NAME missing or empty');
-                    process.exit(1);
-                }
-                Initializer.setECCurveName(response.EC_CURVE_NAME);
-            } catch (err) {
-                console.error('ERROR: Failed to fetch EC_CURVE_NAME from API:', err.message);
-                process.exit(1);
-            }
       }
     };
   })();
